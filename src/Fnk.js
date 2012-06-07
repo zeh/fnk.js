edc5a62ca61f5f9f42fc032358316f6c9ab75ff0
Zeh Fernando <zeh@zehfernando.com>
2012-06-07 15:53:04 -0400

    Added warn() function

--- src/Fnk.js
+++ src/Fnk.js
@@ -19,10 +19,15 @@ FNK.getFormattedTime = function() {
 	return "[" + ("00000000" + FNK.getTimer()).substr(-6, 6) + "] " 
 };
 
-FNK.error = function(__message) {
-	console.error(FNK.getFormattedTime() + __message);
-};
-
 FNK.log = function(__message) {
 	console.log(FNK.getFormattedTime() + __message);
+};
+
+
+FNK.warn = function(__message) {
+	console.warn(FNK.getFormattedTime() + __message);
+};
+
+FNK.error = function(__message) {
+	console.error(FNK.getFormattedTime() + __message);
 };
